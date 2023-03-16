f268a16731e6731fd628beb3c21dfea358bbe2ae
oryrosin <oooory@gmail.com>
2023-03-16 12:27:34 +0200

    fixes on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { ReactComponent as Arrow } from "../assets/drop-arrow.svg";
 import { ReactComponent as Profile } from "../assets/profile-circle.svg";
+import { ReactComponent as ProfileIcon } from "../assets/icons/profile-negative-icon.svg";
 import Logo from "../assets/logoes/logo.png";
 
 import { Button, Text } from "./Reusble";
+import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const [graphMenue, setGraphMenue] = React.useState(false);
+  const navigate = useNavigate();
+
+  const page = window.location.pathname;
+
   return (
     <div>
       <nav className="bg-white px-16 py-2.5 fixed w-full z-20 top-0 left-0 shadow-lg ">
         <div className=" flex flex-wrap items-center justify-between w-full ">
-          <div className="flex items-center justify-between w-2/4">
-            <div className="flex gap-3 items-center">
+          <div className="flex items-center justify-between">
+            <div className="logo-right flex gap-3 items-center">
               <img src={Logo} alt="" />
               <div className=" border-r border-black h-10 "></div>
               <Text sm color="grayHeavy">
@@ -20,73 +26,82 @@ export default function Navbar() {
                 High Level <br /> Cognition Lab{" "}
               </Text>
             </div>
-            <div
-              className="items-center justify-between flex"
-              id="navbar-sticky">
-              <ul className="flex flex-row text-black space-x-12">
-                <li className="flex items-center relative">
-                  <button
-                    class="flex items-center text-white border-0 "
-                    onClick={() => setGraphMenue(!graphMenue)}>
-                    Graphs
-                    <Arrow />
-                  </button>
-
-                  {graphMenue && (
-                    <div
-                      id="dropdownNavbar"
-                      class="z-10  bg-white rounded-md shadow w-44 absolute top-10">
-                      <ul
-                        class="py-2 text-sm text-gray-700 dark:text-gray-400"
-                        aria-labelledby="dropdownLargeButton">
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
-                            Free Queries
-                          </a>
-                          <hr className="mx-3" />
-                        </li>
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
-                            Settings
-                          </a>
-                          <hr className="mx-3" />
-                        </li>
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
-                            Earnings
-                          </a>
-                          <hr className="mx-3" />
-                        </li>
-                        <li>
-                          <a href="#" class="block px-4 py-2 hover:bg-gray-100">
-                            Earnings
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  )}
-                </li>
-                <li>
-                  <a href="/about">About</a>
-                </li>
-                <li>
-                  <a href="#" aria-current="page">
-                    Help
-                  </a>
-                </li>
-                <li>
-                  <a href="#" aria-current="page">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
           </div>
-          <div className="flex flex-row items-center justify-between space-x-4 text-black">
+          <div className="navigate-buttons items-center justify-between flex">
+            <ul className="flex flex-row text-black space-x-12">
+              <li className="flex items-center relative">
+                <button
+                  class="flex items-center text-black border-0 gap-1 "
+                  onClick={() => setGraphMenue(!graphMenue)}>
+                  Explore
+                  <Arrow />
+                </button>
+
+                {graphMenue && (
+                  <div
+                    id="dropdownNavbar"
+                    class="z-10  bg-white rounded-md shadow w-44 absolute top-10">
+                    <ul
+                      class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                      aria-labelledby="dropdownLargeButton">
+                      <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                          Free Queries
+                        </a>
+                        <hr className="mx-3" />
+                      </li>
+                      <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                          Settings
+                        </a>
+                        <hr className="mx-3" />
+                      </li>
+                      <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                          Earnings
+                        </a>
+                        <hr className="mx-3" />
+                      </li>
+                      <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-gray-100">
+                          Earnings
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </li>
+              <li>
+                <a
+                  className={page === "/about" ? "text-blue font-bold" : ""}
+                  href="about">
+                  About
+                </a>
+              </li>
+
+              <li>
+                <a
+                  className={page === "/contact" ? "text-blue font-bold" : ""}
+                  href="contact"
+                  aria-current="page">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a href="#" aria-current="page">
+                  Upload New Paper
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="login-register flex flex-row items-center justify-between space-x-4 text-black">
             {/* <div className=" w-6 h-6 bg-slate-400 rounded-full"></div> */}
             <Profile />
             <span>Login</span>
-            <Button type="button">Submit Research</Button>
+            <Button type="button">
+              {" "}
+              <ProfileIcon /> Register
+            </Button>
           </div>
         </div>
       </nav>
