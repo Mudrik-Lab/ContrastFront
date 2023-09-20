7da28c0a062ffd798e0f51613936833d8e34ae85
oryrosin <oooory@gmail.com>
2023-09-20 08:21:09 +0300

    tryin to fix interpretations bug

--- src/pages/UploadNewPaper/ExperimentsSection/Interpretations.jsx
+++ src/pages/UploadNewPaper/ExperimentsSection/Interpretations.jsx
@@ -24,14 +24,21 @@ export default function Interpretations({
   study_pk,
   values,
 }) {
-  const [fieldValues, setFieldValues] = useState(
-    fieldOptions.map((fieldOption) => {
+  const sortedOptions = fieldOptions
+    .map((fieldOption) => {
       return {
         theory: fieldOption.value,
         type: "",
       };
     })
-  );
+    .sort((a, b) => {
+      if (a.parentTheoryId > b.parentTheoryId) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  const [fieldValues, setFieldValues] = useState(sortedOptions);
   const classificationName = "interpretations";
 
   const handleSubmit = SubmitClassificationField(
@@ -74,66 +81,58 @@ export default function Interpretations({
           }
         />
       }>
-      {fieldValues
-        .sort((a, b) => {
-          if (a.parentTheoryId > b.parentTheoryId) {
-            return 1;
-          } else {
-            return -1;
-          }
-        })
-        .map((fieldValue, index) => {
-          return (
-            <div key={`${classificationName}-${index}`}>
-              <form className="flex flex-col gap-2">
-                <div className="flex gap-2 items-center border border-blue border-x-4 p-2 rounded-md">
-                  <CircledIndex index={index} />
-                  <div className="w-full flex gap-2 items-start">
-                    <div className="w-full">
-                      <Text weight={"bold"} color={"grayReg"}>
-                        Theory
-                      </Text>
-                      <input
-                        readOnly
-                        className="p-2 text-base w-full bg-white disabled:bg-grayDisable border border-gray-300 rounded-md shadow-sm focus:ring-black focus:border-black"
-                        value={
-                          fieldOptions.find(
-                            (option) => option.value === fieldValue.theory
-                          ).label
-                        }
-                      />
-                    </div>
-
-                    <div className="w-full">
-                      <Text weight={"bold"} color={"grayReg"}>
-                        Type
-                      </Text>
-
-                      <CustomSelect
-                        value={fieldValue.type}
-                        onChange={(value) => {
-                          const newArray = [...fieldValues];
-                          newArray[index].type = value;
-                          setFieldValues(newArray);
-                          handleSubmit(fieldValues, index);
-                        }}
-                        options={interpretationTypes}
-                      />
-                    </div>
+      {fieldValues.map((fieldValue, index) => {
+        return (
+          <div key={`${classificationName}-${index}`}>
+            <form className="flex flex-col gap-2">
+              <div className="flex gap-2 items-center border border-blue border-x-4 p-2 rounded-md">
+                <CircledIndex index={index} />
+                <div className="w-full flex gap-2 items-start">
+                  <div className="w-full">
+                    <Text weight={"bold"} color={"grayReg"}>
+                      Theory
+                    </Text>
+                    <input
+                      readOnly
+                      className="p-2 text-base w-full bg-white disabled:bg-grayDisable border border-gray-300 rounded-md shadow-sm focus:ring-black focus:border-black"
+                      value={
+                        fieldOptions.find(
+                          (option) => option.value === fieldValue.theory
+                        ).label
+                      }
+                    />
                   </div>
-                  <div id="trash+submit" className=" flex gap-2">
-                    {/* <SubmitButton
+
+                  <div className="w-full">
+                    <Text weight={"bold"} color={"grayReg"}>
+                      Type
+                    </Text>
+
+                    <CustomSelect
+                      value={fieldValue.type}
+                      onChange={(value) => {
+                        const newArray = [...fieldValues];
+                        newArray[index].type = value;
+                        setFieldValues(newArray);
+                        handleSubmit(fieldValues, index);
+                      }}
+                      options={interpretationTypes}
+                    />
+                  </div>
+                </div>
+                <div id="trash+submit" className=" flex gap-2">
+                  {/* <SubmitButton
                       submit={() => {
                         handleSubmit(fieldValues, index);
                       }}
                       disabled={!(fieldValue?.theory && fieldValue?.type)}
                     /> */}
-                  </div>
                 </div>
-              </form>
-            </div>
-          );
-        })}
+              </div>
+            </form>
+          </div>
+        );
+      })}
     </ExpandingBox>
   );
 }
