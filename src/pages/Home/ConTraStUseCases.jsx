5387f2268852b93dbee2c720f631d4018aaa15c1
oryrosin <oooory@gmail.com>
2023-09-29 08:40:02 +0300

    drop vimeo try

--- src/pages/Home/ConTraStUseCases.jsx
+++ src/pages/Home/ConTraStUseCases.jsx
@@ -7,7 +7,8 @@ import Cogtate from "../../assets/logoes/cogtate.png";
 export default function ConTraStUseCases() {
   const cardsInfo = [
     {
-      image: "https://player.vimeo.com/video/274353008?h=a5e71d0bc5",
+      image:
+        "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&quality=85&auto=format&fit=max&s=a52bbe202f57ac0f5ff7f47166906403",
       headline: "Find the papers that are most relevant to you",
       text: "Conduct your own queries, looking for different intersections of methods, approaches and paradigms. Download citations lists of relevant works that could help you map the field and get started!",
     },
