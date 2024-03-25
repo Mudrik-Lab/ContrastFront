517631bcf3ae4432f76a1fe1ca8721634c96d6e6
Chen Peleg <chenpeleg@gmail.com>
2024-03-25 11:01:54 +0200

    contrast and uncontrast folders

--- documentation/two-projects-same-src.md
+++ documentation/two-projects-same-src.md
@@ -7,3 +7,5 @@ We want the projects to be independent of each other, at some level but to share
 ### References
 
 - [Multiple projects in the same source directory](https://stackoverflow.com/questions/74159670/vite-multiple-apps-with-same-source)
+- [Vite Docs](https://vitejs.dev/guide/features.html#multi-page-app)
+- [build-javascript-library-with-multiple-entry-points-using-vite-3](https://www.raulmelo.me/en/blog/build-javascript-library-with-multiple-entry-points-using-vite-3)
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import {resolve} from 'path';
+
+const configs = {
+  contrast: {
+    entry: resolve(__dirname, "./src/contrast/main-contrast.jsx"),
+    fileName: "main.js",
+  },
+  uncontrast: {
+    entry: resolve(__dirname, "./src/uncontrast/main-uncontrast.jsx"),
+    fileName: "main.js",
+  },
+};
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
 });
+
+
