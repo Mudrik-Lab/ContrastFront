18849461cd360b9fe089bd891ff85376da9c4ca7
Chen Peleg <chenpeleg@gmail.com>
2024-03-25 14:21:47 +0200

    added inputs

--- vite.config.js
+++ vite.config.js
@@ -2,48 +2,48 @@ import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import {resolve} from 'path';
- import fs from "node:fs/promises";
+import fs from "node:fs/promises";
 
 export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
-
     const defaultConfig = {
-        plugins: [react(), svgr(),   {
-                name: 'index-html-build-replacement',
-                apply: 'serve' ,
-                //   docs:
-                //   https://vitejs.dev/guide/api-plugin.html#conditional-application
-                   async transformIndexHtml(html) {
-                    if (mode === 'contrast_prod') {
-                         const htmlFile = await fs.readFile('./index-contrast.html', 'utf8')
-                        return htmlFile;
-                    }
+        plugins: [react(), svgr(), {
+            name: 'index-html-build-replacement', apply: 'serve', //   docs:
+            //   https://vitejs.dev/guide/api-plugin.html#conditional-application
+            async transformIndexHtml(html) {
+                switch (mode) {
+                    case  'contrast_prod':
+                        return await fs.readFile('./index-contrast.html', 'utf8');
 
-                    return  html;
+                    case 'uncontrast_prod':
+                        return await fs.readFile('./index-uncontrast.html', 'utf8');
                 }
+
+                return html;
             }
-            ],
+        }],
     };
     switch (mode) {
         case "contrast_prod":
             return {
                 ...defaultConfig, build: {
                     rollupOptions: {
+                        output: {
+                            name : 'index'
+
+                        },
                         input: resolve(__dirname, './index-contrast.html'),
-                    },
-                    outDir: 'dist-contrast',
+                    }, outDir: 'dist-contrast',
                 },
 
             };
 
         case "uncontrast_prod":
             return {
-                ...defaultConfig,
-                build: {
+                ...defaultConfig, build: {
                     rollupOptions: {
                         input: resolve(__dirname, './index-uncontrast.html'),
 
-                    },
-                    outDir: 'dist-uncontrast',
+                    }, outDir: 'dist-uncontrast',
 
                 },
 
@@ -52,8 +52,6 @@ export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
         default:
             return defaultConfig;
     }
-
-
 });
 
 
