c3bc0d51c137ea93a45f16cf8250272ad8a6297f
Chen Peleg <chenpeleg@gmail.com>
2024-03-27 09:42:31 +0200

    added two main jsx files

---
+++ src/uncontrast/main-uncontrast.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "../App";
+import "../index.css";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+
+// Sanity check : TODO: Remove
+console.log( `You are at site: ${Site.type}, in main-uncontrast.jsx`);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
