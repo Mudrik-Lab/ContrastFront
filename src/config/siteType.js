f578994059a12ad613009734c1a75ff3f9114698
Chen Peleg <chenpeleg@gmail.com>
2024-03-31 11:55:16 +0300

    added dev envs

--- src/config/siteType.js
+++ src/config/siteType.js
@@ -4,12 +4,26 @@
 export class Site {
     static contrast = "contrast";
     static unContrast = "uncontrast";
+    static get isDev () {
+        switch (import.meta.env.VITE_MODE_NAME) {
+            case "contrast_prod":
+            case "uncontrast_prod":
+                return false;
+            case "contrast_dev":
+            case "uncontrast_dev":
+                return false;
+            default:
+                return false;
+        }
+    }
 
     static get type() {
         switch (import.meta.env.VITE_MODE_NAME) {
             case "contrast_prod":
+            case "contrast_dev":
                 return Site.contrast;
             case "uncontrast_prod":
+            case "uncontrast_dev":
                 return Site.unContrast;
             default:
                 return Site.contrast;
