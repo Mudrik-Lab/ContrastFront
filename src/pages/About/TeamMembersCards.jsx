6b6a22be082d9395e0f105d5b54f4576ec4d1907
oryrosin <oooory@gmail.com>
2024-04-01 13:54:51 +0300

    About page

--- src/pages/About/TeamMembersCards.jsx
+++ src/pages/About/TeamMembersCards.jsx
@@ -1,115 +1,115 @@
 import React from "react";
-import { Text } from "../.././sharedComponents/Reusble";
+import {Text} from "../.././sharedComponents/Reusble";
 import ShowMoreText from "react-show-more-text";
-import { ReactComponent as Web } from "../../assets/icons/website.svg";
+import {ReactComponent as Web} from "../../assets/icons/website.svg";
 import karine from "../../assets/images/Karine.jpg";
-import { foundingBoardBios } from "../../Utils/FoundersBios";
+import {foundingBoardBios} from "../../Utils/FoundersBios";
 
 export default function TeamMembersCards() {
-  return (
-    <div>
-      <div className="flex flex-wrap justify-center sm:w-[900px] sm:px-24 my-6 gap-5 gap-y-7 mx-auto ">
-        {foundingBoardBios
-          .sort((a, b) => {
-            if (a.secondName < b.secondName) {
-              return -1;
-            }
-            if (a.secondName > b.secondName) {
-              return 1;
-            }
-            return 0;
-          })
-          .map((card) => (
-            <div
-              className="team-member-card w-40 sm:w-48 flex flex-col items-center shadow-lg p-2 rounded-lg relative"
-              key={card.name}>
-              <div
-                style={{
-                  width: 120,
-                  height: 120,
-                  borderRadius: "50%",
-                  backgroundImage: `url(${card.image})`,
-                  backgroundPosition: "center",
-                  backgroundSize: "cover",
-                  backgroundRepeat: "no-repeat",
-                }}></div>
+    return (
+        <div>
+            <div className="flex flex-wrap justify-center sm:w-[900px] sm:px-24 my-6 gap-5 gap-y-7 mx-auto ">
+                {foundingBoardBios.filter(member => member.isContrastMember === true)
+                    .sort((a, b) => {
+                        if (a.secondName < b.secondName) {
+                            return -1;
+                        }
+                        if (a.secondName > b.secondName) {
+                            return 1;
+                        }
+                        return 0;
+                    })
+                    .map((card) => (
+                        <div
+                            className="team-member-card w-40 sm:w-48 flex flex-col items-center shadow-lg p-2 rounded-lg relative"
+                            key={card.name}>
+                            <div
+                                style={{
+                                    width: 120,
+                                    height: 120,
+                                    borderRadius: "50%",
+                                    backgroundImage: `url(${card.image})`,
+                                    backgroundPosition: "center",
+                                    backgroundSize: "cover",
+                                    backgroundRepeat: "no-repeat",
+                                }}></div>
 
-              <div className="min-h-[180px] max-h-[260px]">
-                <div className="flex flex-col justify-center items-center my-4">
-                  <Text center lg weight="bold">
-                    {card.name}
-                  </Text>
+                            <div className="min-h-[180px] max-h-[260px]">
+                                <div className="flex flex-col justify-center items-center my-4">
+                                    <Text center lg weight="bold">
+                                        {card.name}
+                                    </Text>
+                                </div>
+
+                                <ShowMoreText
+                                    className="text-sm text-grayHeavy break-keep text-center mb-10"
+                                    lines={1}
+                                    more="Show more"
+                                    less="Show less"
+                                    anchorClass="show-more-less-clickable"
+                                    width={280}
+                                    expanded={false}
+                                    textStyle={{textAlign: "center"}}
+                                    truncatedEndingComponent={"... "}>
+                                    {card.text}
+                                </ShowMoreText>
+                            </div>
+                            <div className="absolute  bottom-2">
+                                <a
+                                    href={card.website}
+                                    aria-label="link to member's website"
+                                    target="_blank">
+                                    <Web/>
+                                </a>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+            <div className="flex justify-center mt-24 mx-auto ">
+                <div className="Karine's-card w-48 flex flex-col items-center shadow-lg p-2 rounded-lg relative">
+                    <div
+                        style={{
+                            width: 120,
+                            height: 120,
+                            borderRadius: "50%",
+                            backgroundImage: `url(${karine})`,
+                            backgroundPosition: "center",
+                            backgroundSize: "cover",
+                            backgroundRepeat: "no-repeat",
+                        }}></div>
+
+                    <div className="w-[150px]">
+                        <div className="flex flex-col justify-center items-center my-4">
+                            <Text center lg weight="bold">
+                                {"Karine Jospe"}
+                            </Text>
+                        </div>
+
+                        <ShowMoreText
+                            className="text-sm text-grayHeavy break-keep text-center mb-10"
+                            lines={1}
+                            more="Show more"
+                            less="Show less"
+                            anchorClass="show-more-less-clickable"
+                            width={280}
+                            expanded={false}
+                            textStyle={{textAlign: "center"}}
+                            truncatedEndingComponent={"... "}>
+                            Website Manager
+                        </ShowMoreText>
+                    </div>
+                    <div className="absolute  bottom-2">
+                        <a
+                            href={
+                                "https://people.socsci.tau.ac.il/mu/mudriklab/people/#gkit-popup"
+                            }
+                            aria-label="link to Karine's website"
+                            target="_blank">
+                            <Web/>
+                        </a>
+                    </div>
                 </div>
-
-                <ShowMoreText
-                  className="text-sm text-grayHeavy break-keep text-center mb-10"
-                  lines={1}
-                  more="Show more"
-                  less="Show less"
-                  anchorClass="show-more-less-clickable"
-                  width={280}
-                  expanded={false}
-                  textStyle={{ textAlign: "center" }}
-                  truncatedEndingComponent={"... "}>
-                  {card.text}
-                </ShowMoreText>
-              </div>
-              <div className="absolute  bottom-2">
-                <a
-                  href={card.website}
-                  aria-label="link to member's website"
-                  target="_blank">
-                  <Web />
-                </a>
-              </div>
             </div>
-          ))}
-      </div>
-      <div className="flex justify-center mt-24 mx-auto ">
-        <div className="Karine's-card w-48 flex flex-col items-center shadow-lg p-2 rounded-lg relative">
-          <div
-            style={{
-              width: 120,
-              height: 120,
-              borderRadius: "50%",
-              backgroundImage: `url(${karine})`,
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-            }}></div>
-
-          <div className="w-[150px]">
-            <div className="flex flex-col justify-center items-center my-4">
-              <Text center lg weight="bold">
-                {"Karine Jospe"}
-              </Text>
-            </div>
-
-            <ShowMoreText
-              className="text-sm text-grayHeavy break-keep text-center mb-10"
-              lines={1}
-              more="Show more"
-              less="Show less"
-              anchorClass="show-more-less-clickable"
-              width={280}
-              expanded={false}
-              textStyle={{ textAlign: "center" }}
-              truncatedEndingComponent={"... "}>
-              Website Manager
-            </ShowMoreText>
-          </div>
-          <div className="absolute  bottom-2">
-            <a
-              href={
-                "https://people.socsci.tau.ac.il/mu/mudriklab/people/#gkit-popup"
-              }
-              aria-label="link to Karine's website"
-              target="_blank">
-              <Web />
-            </a>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/uncontrast/UncontrastScreen.jsx
+++ src/uncontrast/UncontrastScreen.jsx
@@ -1,42 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import * as React from "react";
 import Homepage from "./pages/Home/Homepage.jsx";
+import Aboutpage from "./pages/About/Aboutpage.jsx";
 
 const UncontrastScreens = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/login" element={<Login />} />
-        <Route
-          path="/request-password-recovery"
-          element={<RecoverPassword />}
-        />
-        <Route path="/reset_password" element={<ResetPassword />} /> */}
-          {/*<Route path="/" element={<h1>this is uncontrast on monoripo</h1>} />*/}
-          <Route path="/" element={<Homepage />}/>
+    return (
+        <BrowserRouter>
+            <Routes>
 
-        {/* <Route path="/about" element={<AboutPage />} />
-        <Route path="/terms-of-use" element={<TermOfUse />} />
-        <Route path="/modes-of-governance" element={<ModesOfGoverance />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute
-              path="/profile"
-              element={
-                <React.Suspense fallback={<>...</>}>
-                  <SecondaryRegister />
-                </React.Suspense>
-              }
-            />
-          }
-        />
-      */}
-      </Routes>
-    </BrowserRouter>
-  );
+                <Route path="/" element={<Homepage/>}/>
+                <Route path="/about" element={<Aboutpage/>}/>
+
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default UncontrastScreens;
