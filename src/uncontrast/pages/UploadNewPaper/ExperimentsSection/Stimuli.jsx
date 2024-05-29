b85b687712fa84d73288465011b63710f7700c49
oryrosin <oooory@gmail.com>
2024-05-29 16:01:00 +0300

    stimuli additions

--- src/uncontrast/pages/UploadNewPaper/ExperimentsSection/Stimuli.jsx
+++ src/uncontrast/pages/UploadNewPaper/ExperimentsSection/Stimuli.jsx
@@ -30,6 +30,9 @@ export default function Stimuli({
     category: "",
     modality: "",
     duration: "",
+    stimuliNum: "",
+    SOA: "",
+    mode_of_presentation: "",
   };
   const [fieldValues, setFieldValues] = useState([initialValues]);
   const classificationName = "stimuli";
@@ -65,6 +68,9 @@ export default function Stimuli({
             sub_category: row.sub_category || "",
             modality: row.modality,
             duration: row.duration,
+            mode_of_presentation: row.mode_of_presentation,
+            SOA: row.SOA,
+            stimuliNum: row.stimuliNum,
             id: row.id,
           };
         })
@@ -76,7 +82,10 @@ export default function Stimuli({
     return (
       fieldValues[index]?.category &&
       fieldValues[index]?.modality &&
-      fieldValues[index]?.duration
+      fieldValues[index]?.duration &&
+      fieldValues[index]?.mode_of_presentation &&
+      fieldValues[index]?.stimuliNum &&
+      fieldValues[index]?.SOA
     );
   };
 
@@ -90,7 +99,7 @@ export default function Stimuli({
   return (
     <ExpandingBox
       number={fieldsNum}
-      disabled={disabled}
+      disabled={false} // temporal false
       headline={rawTextToShow(classificationName)}>
       {fieldValues.map((fieldValue, index) => {
         return (
@@ -228,27 +237,145 @@ export default function Stimuli({
                       </div>
                     </div>
                   </div>
+                  <div className="flex items-start gap-2">
+                    <div>
+                      <TooltipExplanation
+                        isHeadline
+                        tooltip={
+                          "Enter the number of different suppressed stimuli that were used in the experiment; e.g., if the suppressed stimuli were the digits between 1-9, the number is 9. If this information is not available,  leave empty."
+                        }
+                        text={"Used stimuli on experiment"}
+                      />
+                      <div className="flex flex-col items-center">
+                        <input
+                          min={0}
+                          disabled={fieldValues[index]?.id}
+                          type="number"
+                          value={fieldValues[index].stimuliNum}
+                          onChange={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            setFieldValues((prev) =>
+                              prev.map((item, i) =>
+                                i === index
+                                  ? { ...item, stimuliNum: e.target.value }
+                                  : item
+                              )
+                            );
+                          }}
+                          onBlur={(e) => {
+                            e.stopPropagation();
+                            if (submitCondition(index)) {
+                              e.preventDefault();
+
+                              handleSubmit(fieldValues, index);
+                            }
+                          }}
+                          onKeyDown={(e) => {
+                            e.stopPropagation();
+
+                            if (e.key === "Enter" && submitCondition(index)) {
+                              e.preventDefault();
+                              handleSubmit(fieldValues, index);
+                            } else if (e.key === "Enter") {
+                              e.preventDefault();
+                            }
+                          }}
+                          className={`border w-full border-gray-300 rounded-md p-2 ${
+                            fieldValues[index].id &&
+                            "bg-grayDisable text-gray-400"
+                          } `}
+                        />
+                      </div>
+                    </div>
+                    <div className="w-20">
+                      <TooltipExplanation
+                        isHeadline
+                        tooltip={
+                          "Enter the time between the appearance of the suppressed stimulus and appearance of the non-suppressed stimulus.If information is not available or irrelevant, leave empty"
+                        }
+                        text={"SOA (ms)"}
+                      />
+                      <div className="flex flex-col items-center">
+                        <input
+                          min={0}
+                          disabled={fieldValues[index]?.id}
+                          type="number"
+                          value={fieldValues[index].SOA}
+                          onChange={(e) => {
+                            e.stopPropagation();
+                            e.preventDefault();
+                            setFieldValues((prev) =>
+                              prev.map((item, i) =>
+                                i === index
+                                  ? { ...item, SOA: e.target.value }
+                                  : item
+                              )
+                            );
+                          }}
+                          onBlur={(e) => {
+                            e.stopPropagation();
+                            if (submitCondition(index)) {
+                              e.preventDefault();
+
+                              handleSubmit(fieldValues, index);
+                            }
+                          }}
+                          onKeyDown={(e) => {
+                            e.stopPropagation();
+
+                            if (e.key === "Enter" && submitCondition(index)) {
+                              e.preventDefault();
+                              handleSubmit(fieldValues, index);
+                            } else if (e.key === "Enter") {
+                              e.preventDefault();
+                            }
+                          }}
+                          className={`border w-full border-gray-300 rounded-md p-2 ${
+                            fieldValues[index].id &&
+                            "bg-grayDisable text-gray-400"
+                          } `}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <TooltipExplanation
+                        isHeadline
+                        tooltip={
+                          "If the suppressed stimuli in the experiment were presented at detection threshold, as determined by a calibration procedure, enter “liminal”. Otherwise, enter “subliminal”"
+                        }
+                        text={"Mode of presentation"}
+                      />
+                      <CustomSelect
+                        disabled={fieldValue.id}
+                        value={fieldValue.mode_of_presentation}
+                        onChange={(value) => {
+                          const newArray = [...fieldValues];
+                          newArray[index].mode_of_presentation = value;
+                          setFieldValues(newArray);
+                          submitCondition(index) &&
+                            handleSubmit(fieldValues, index);
+                        }}
+                        options={[
+                          { value: "liminal", label: "Liminal" },
+                          { value: "subliminal", label: "Subliminal" },
+                        ]}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-start gap-2">
+                    TODO: add Are there also non-suppressed stimuli that
+                    participants had to provide a response to (i.e., a target)?
+                    Dropdown menu: yes/no
+                  </div>
                 </div>
-                <div className="border-r-2 border-blue h-24"></div>
+                <div className="border-r-2 border-blue h-36"></div>
                 <div id="trash+submit">
                   <TrashButton
                     handleDelete={handleDelete}
                     fieldValues={fieldValues}
                     index={index}
                   />
-                  {/* <SubmitButton
-                    submit={() => {
-                      handleSubmit(fieldValues, index);
-                    }}
-                    disabled={
-                      !(
-                        fieldValue?.category &&
-                        fieldValue?.sub_category &&
-                        fieldValue?.duration &&
-                        fieldValue?.modality
-                      ) || fieldValue.id
-                    }
-                  /> */}
                 </div>
               </div>
             </form>
