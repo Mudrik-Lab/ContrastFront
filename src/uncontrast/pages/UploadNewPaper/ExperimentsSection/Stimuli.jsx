284edcd362b269885f62a07dbaaf4f54336d3ffa
oryrosin <oooory@gmail.com>
2024-06-05 14:31:19 +0300

    stimuli new structure

--- src/uncontrast/pages/UploadNewPaper/ExperimentsSection/Stimuli.jsx
+++ src/uncontrast/pages/UploadNewPaper/ExperimentsSection/Stimuli.jsx
@@ -28,11 +28,18 @@ export default function Stimuli({
 }) {
   const initialValues = {
     category: "",
+    sub_category: "",
     modality: "",
     duration: "",
     stimuliNum: "",
     SOA: "",
     mode_of_presentation: "",
+    are_also_nonsuppressed: "",
+    same_as_suppressed: "",
+    nonsuppressedCategory: "",
+    nonsuppressedSubcategory: "",
+    nonsuppressedModality: "",
+    nonsuppressedDuration: "",
   };
   const [fieldValues, setFieldValues] = useState([initialValues]);
   const classificationName = "stimuli";
@@ -71,6 +78,8 @@ export default function Stimuli({
             mode_of_presentation: row.mode_of_presentation,
             SOA: row.SOA,
             stimuliNum: row.stimuliNum,
+            are_also_nonsuppressed: row.are_also_nonsuppressed,
+            nonsuppressedCategory: row.nonsuppressedCategory,
             id: row.id,
           };
         })
@@ -96,6 +105,7 @@ export default function Stimuli({
       stimuli: fieldsNum,
     });
   }, [fieldsNum]);
+
   return (
     <ExpandingBox
       number={fieldsNum}
@@ -364,10 +374,223 @@ export default function Stimuli({
                     </div>
                   </div>
                   <div className="flex items-start gap-2">
-                    TODO: add Are there also non-suppressed stimuli that
-                    participants had to provide a response to (i.e., a target)?
-                    Dropdown menu: yes/no
+                    <div className="flex gap-2 w-full items-center">
+                      <div className="w-2/3">
+                        <Text weight={"bold"} color={"grayReg"}>
+                          Are there also non-suppressed stimuli that
+                          participants had to provide a response to (i.e., a
+                          target)?
+                        </Text>
+                      </div>
+                      <div className="w-1/3 flex justify-between items-center gap-2">
+                        <CustomSelect
+                          disabled={fieldValue?.id}
+                          value={fieldValue.are_also_nonsuppressed}
+                          onChange={(value) => {
+                            const newArray = [...fieldValues];
+                            newArray[index].are_also_nonsuppressed = value;
+                            setFieldValues(newArray);
+                          }}
+                          options={[
+                            { value: "yes", label: "Yes" },
+                            { value: "no", label: "No" },
+                          ]}
+                        />
+                        <TooltipExplanation
+                          isHeadline
+                          tooltip={
+                            " In most experiments, participants are asked to respond to non suppressed stimuli. If this is the case, enter “yes”"
+                          }
+                        />
+                      </div>
+                    </div>
                   </div>
+                  {fieldValue.are_also_nonsuppressed === "yes" && (
+                    <div>
+                      <div className="flex items-start gap-2">
+                        <div className="flex gap-2 w-full items-center">
+                          <div className="w-2/3">
+                            <Text weight={"bold"} color={"grayReg"}>
+                              Is the non-suppressed stimulus the same as the
+                              suppressed stimulus?
+                            </Text>
+                          </div>
+                          <div className="w-1/3 flex justify-between items-center gap-2">
+                            <CustomSelect
+                              disabled={fieldValue?.id}
+                              value={fieldValue.same_as_suppressed}
+                              onChange={(value) => {
+                                const newArray = [...fieldValues];
+                                newArray[index].same_as_suppressed = value;
+                                setFieldValues(newArray);
+                              }}
+                              options={[
+                                { value: "yes", label: "Yes" },
+                                { value: "no", label: "No" },
+                              ]}
+                            />
+                            <TooltipExplanation
+                              isHeadline
+                              tooltip={
+                                " In most experiments, participants are asked to respond to non suppressed stimuli. If this is the case, enter “yes”"
+                              }
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      {fieldValue.same_as_suppressed === "no" && (
+                        <div className="px-2 py-1 border-2 border-blue rounded-md bg-white mt-2">
+                          <Text center weight={"bold"}>
+                            Non-suppressed stimuli
+                          </Text>
+                          <div className="flex items-start gap-2 mt-2">
+                            <div className="w-full flex gap-2 items-start">
+                              <div className="w-full">
+                                <div className="flex gap-1 items-center">
+                                  <Text weight={"bold"} color={"grayReg"}>
+                                    Category
+                                  </Text>
+                                  <TooltipExplanation
+                                    isHeadline
+                                    tooltip={
+                                      "Indicate the category of the non-suppressed stimuli used in the experiment. You may choose more than one option"
+                                    }
+                                  />
+                                </div>
+
+                                <CustomSelect
+                                  disabled={fieldValue.id}
+                                  value={fieldValue.nonsuppressedCategory}
+                                  onChange={(value) => {
+                                    const newArray = [...fieldValues];
+                                    newArray[index].nonsuppressedCategory =
+                                      value;
+                                    setFieldValues(newArray);
+                                    submitCondition(index) &&
+                                      handleSubmit(fieldValues, index);
+                                  }}
+                                  options={fieldOptions}
+                                />
+                              </div>
+                              <div className="w-full">
+                                <Text
+                                  weight={"bold"}
+                                  color={"grayReg"}
+                                  className={"whitespace-nowrap "}>
+                                  Sub-category (optional)
+                                </Text>
+                                <CustomSelect
+                                  disabled={fieldValue.id}
+                                  defaultValue={
+                                    fieldValue.nonsuppressedSubcategory
+                                  }
+                                  onChange={(value) => {
+                                    const newArray = [...fieldValues];
+                                    value !== ""
+                                      ? (newArray[
+                                          index
+                                        ].nonsuppressedSubcategory = value)
+                                      : delete newArray[index]
+                                          .nonsuppressedSubcategory;
+                                    setFieldValues(newArray);
+                                    submitCondition(index) &&
+                                      handleSubmit(fieldValues, index);
+                                  }}
+                                  options={alphabetizeByLabels(subCategories)}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="flex items-start gap-2">
+                            <div className="w-full">
+                              <TooltipExplanation
+                                isHeadline
+                                tooltip={
+                                  "Indicate in which modality the stimuli were presented."
+                                }
+                                text={"Modality"}
+                              />
+                              <CustomSelect
+                                disabled={fieldValue.id}
+                                value={fieldValue.nonsuppressedModality}
+                                onChange={(value) => {
+                                  const newArray = [...fieldValues];
+                                  newArray[index].nonsuppressedModality = value;
+                                  setFieldValues(newArray);
+                                  submitCondition(index) &&
+                                    handleSubmit(fieldValues, index);
+                                }}
+                                options={modalities}
+                              />
+                            </div>
+                            <div className="w-20">
+                              <TooltipExplanation
+                                isHeadline
+                                tooltip={
+                                  "Enter the presentation duration of the critical stimulus in ms. If this information is not available, enter NA."
+                                }
+                                text={"Duration"}
+                              />
+
+                              <div className="flex flex-col items-center">
+                                <input
+                                  min={0}
+                                  disabled={fieldValues[index]?.id}
+                                  type="number"
+                                  value={
+                                    fieldValues[index].nonsuppressedDuration
+                                  }
+                                  onChange={(e) => {
+                                    e.stopPropagation();
+                                    e.preventDefault();
+                                    setFieldValues((prev) =>
+                                      prev.map((item, i) =>
+                                        i === index
+                                          ? {
+                                              ...item,
+                                              nonsuppressedDuration:
+                                                e.target.value,
+                                            }
+                                          : item
+                                      )
+                                    );
+                                  }}
+                                  onBlur={(e) => {
+                                    e.stopPropagation();
+                                    if (submitCondition(index)) {
+                                      e.preventDefault();
+
+                                      handleSubmit(fieldValues, index);
+                                    }
+                                  }}
+                                  onKeyDown={(e) => {
+                                    e.stopPropagation();
+
+                                    if (
+                                      e.key === "Enter" &&
+                                      submitCondition(index)
+                                    ) {
+                                      e.preventDefault();
+                                      handleSubmit(fieldValues, index);
+                                    } else if (e.key === "Enter") {
+                                      e.preventDefault();
+                                    }
+                                  }}
+                                  className={`border w-full border-gray-300 rounded-md p-2 ${
+                                    fieldValues[index].id &&
+                                    "bg-grayDisable text-gray-400"
+                                  } `}
+                                />
+                                <Text xs weight={"bold"} color={"grayReg"}>
+                                  (ms)
+                                </Text>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )}
                 </div>
                 <div className="border-r-2 border-blue h-36"></div>
                 <div id="trash+submit">
