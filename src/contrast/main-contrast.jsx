f9f3d7a366461790cb9a931ac12cb37b365f070d
oryrosin <oooory@gmail.com>
2024-07-09 11:41:17 +0300

    fixed sentry cofig

--- src/contrast/main-contrast.jsx
+++ src/contrast/main-contrast.jsx
@@ -1,7 +1,7 @@
+import * as Sentry from "@sentry/react";
 import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "../index.css";
 import { Site } from "../config/siteType.js";
 import AppContrast from "./AppContrast.jsx";
@@ -16,11 +16,12 @@ Sentry.init({
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
   tracePropagationTargets: [
-    "localhost",
+    "http://localhost:5173/",
     "https://uncontrustdb.tau.ac.il/api/",
     "https://contrastdb.tau.ac.il/api/",
     /^https:\/\/yourserver\.io\/api/,
   ],
+
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
